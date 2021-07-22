a0225d8fb629e38f4b1df5d6dd5db794fea9f189
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-22 18:23:19 +0400

    addition.sh

---
+++ addition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chmod 500 ~/termuxloginpass-iOS/login.py
+chmod 500 ~/termuxloginpass-iOS/pass.py
+chmod 500 ~/termuxloginpass-iOS/chlogin.py
+chmod 500 ~/termuxloginpass-iOS/chpass.py
+chmod 500 ~/termuxloginpass-iOS
