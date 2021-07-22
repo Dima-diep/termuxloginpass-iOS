1fc50fa688ffe66e9a1b719bcd65185fc053204d
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-22 18:25:50 +0400

    install-bash.sh

---
+++ install-bash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "python3 ~/termuxloginpass-iOS/login.py" >> /etc/bash.bashrc
+echo "chmod +x exit.sh && bash exit.sh" >> /etc/bash.bashrc
+echo "python3 ~/termuxloginpass-iOS/login-style.py" >> /etc/bash.bashrc
+mkdir ~/.linuxcolor
+mv kalilinux ~/.linuxcolor
+mv debian ~/.linuxcolor
+mv Ubuntu ~/.linuxcolor
+mv archlinux ~/.linuxcolor
