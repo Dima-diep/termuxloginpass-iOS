bc4d9685e6e3c67325d92aa9e3d0fa3c3bb9579c
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-05 14:09:21 +0400

    install-zsh.sh

--- install-zsh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "python3 ~/termuxloginpass-iOS/login.py" >> /etc/zshrc
-echo "chmod +x exit.sh && bash exit.sh" >> /etc/zshrc
-echo "python3 ~/termuxloginpass-iOS/login-style.py" >> /etc/zshrc
-mkdir ~/.linuxcolor
-mv kalilinux ~/.linuxcolor
-mv debian ~/.linuxcolor
-mv Ubuntu ~/.linuxcolor
-mv archlinux ~/.linuxcolor
