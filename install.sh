c0c426549f179b5e0b2a23ff89a54b567358f043
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-05 21:31:25 +0400

    Update install.sh
    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+echo "#!/bin/bash" > /etc/login
 echo "python3 ~/termuxloginpass-iOS/login.py" >> /etc/login
-echo "chmod +x exit.sh && bash exit.sh" >> /etc/login
 echo "python3 ~/termuxloginpass-iOS/login-style.py" >> /etc/login
 mkdir ~/.linuxcolor
 mv kalilinux ~/.linuxcolor
